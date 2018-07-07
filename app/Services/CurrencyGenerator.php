aa7b5b620df70d47aa9ef742ac684d3eb4ac6789
Alexander Kharchenko <sandrokharchenko@gmail.com>
2018-07-07 11:15:52 +0300

    Implemented class "CurrencyGenerator"

--- app/Services/CurrencyGenerator.php
+++ app/Services/CurrencyGenerator.php
@@ -4,8 +4,34 @@ namespace App\Services;
 
 class CurrencyGenerator
 {
+    const CURRENCY_URL = 'https://api.coinmarketcap.com/v2/ticker/?structure=array';
+    const IMAGE_URL_START = 'https://s2.coinmarketcap.com/static/img/coins/16x16/';
+    
     public static function generate(): array
     {
-        // todo implement
+        $currencies = [];
+        
+        $data = static::fetchCurrencies();
+
+        foreach ($data as $item) {
+            $currency = new Currency($item->id,
+                                     $item->name,
+                                     $item->quotes->USD->price,
+                                     static::IMAGE_URL_START . $item->id . '.png',
+                                     $item->quotes->USD->percent_change_24h);
+            
+            $currencies[] = $currency;
+        }
+
+        return $currencies;
+    }
+
+    private static function fetchCurrencies(): array
+    {
+        $currencies = file_get_contents(static::CURRENCY_URL);
+        $currencies = json_decode($currencies);
+        $currencies = $currencies->data;
+
+        return $currencies;
     }
 }
